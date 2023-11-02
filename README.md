6e76e1b393dc64a360ef0e164fe990fca6770311
LuisZentenxx <luis.zenteno02@inacapmail.cl>
2023-11-02 19:45:17 -0300

    update readme.md

---
+++ README.md
@@ -0,0 +1,30 @@
+# GoWeather
+
+![image](https://ibb.co/gzc5Zwv)
+
+## Descripción
+Este proyecto consiste en el desarrollo de una aplicación de línea de comandos en **Golang** utilizando el framework **Cobra**. Con esta aplicación, los usuarios podrán obtener fácilmente la temperatura actual de cualquier ciudad del mundo simplemente ingresando el nombre de la ciudad como argumento del comando.
+
+## Caracteristicas
+
+- Integración de una API de pronóstico del tiempo para obtener datos de temperatura en tiempo real.
+  https://weatherstack.com/
+  
+- Implementación de la funcionalidad para consultar la temperatura de una ciudad específica.
+- Uso de argumentos de línea de comandos para personalizar las consultas de temperatura.
+- Manejo de errores y excepciones para una experiencia de usuario fluida.
+- Ejemplos prácticos de uso de la aplicación.
+
+## Tecnologías y Funcionalidades
+
+### Tecnologías utilizadas
+
+- Golang
+- Framework Cobra
+
+### Funcionalidades
+
+- Consulta de la temperatura actual de una ciudad específica.
+- Uso de argumentos de línea de comandos para personalizar las consultas.
+- Manejo de errores para una mejor experiencia de usuario.
+  
--- main.go
+++ main.go
@@ -1,12 +1,58 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/spf13/cobra"
 )
 
+type Location struct {
+	Name string `json:"name"`
+}
+
+type Weather struct {
+	Temperature string `json:"temperature"`
+}
+
+type Response struct {
+	Location Location `json:"location"`
+	Current  Weather  `json:"current"`
+}
+
+func getTemperature(str string) string {
+	httpClient := http.Client{}
+	req, err := http.NewRequest("GET", "http://api.weatherstack.com/current", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	q := req.URL.Query()
+	q.Add("acces_key", "0eef548ef1ee12f7d680b1c34c2476b0")
+	q.Add("query", str)
+	req.URL.RawQuery = q.Encode()
+
+	res, err := httpClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()
+}
+
 func main() {
 
-	var rootCmd = &cobra.Command{Use: "app"}
-	rootCmd.Execute()
+	var cmdTemperature = &cobra.Command{
+		Use:   "temperatura [cadena a imprimir]",
+		Short: "Muestra la temperatura de un lugar.",
+		Long:  `Muestra la temperatura de un lugar especificando su nombre como parámetro del comando.`,
+		Args:  cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Mostrar Temperatura")
+		},
+	}
 
+	var rootCmd = &cobra.Command{Use: "app"}
+	rootCmd.AddCommand(cmdTemperature)
+	rootCmd.Execute()
 }
